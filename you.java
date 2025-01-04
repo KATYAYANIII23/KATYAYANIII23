93859ee8175c492ee1128248e40bde8b9cdb149d
KATYAYANIII23 <149229397+KATYAYANIII23@users.noreply.github.com>
2025-01-04 13:46:08 +0530

    Create you.java

---
+++ you.java
@@ -0,0 +1,17 @@
+// creating a parent class
+class vehicle{
+  void run() (system.out.printin)
+}
+class Animal{
+  void eat(){system.out.printin("eating....");}
+}
+class Dog extends Animal{
+class Dog(){system.out.printin("barking...");}
+}
+class Test inheritance {
+  public static void main (string args[]){
+    Dog d=New Dog();
+    d.bark();
+    d.eat();
+  }
+}
